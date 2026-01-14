06d0e75a4426c62856d37d0b2f8d2bfbdd044bcc
Isaac Miti <ikayz@live.com>
2026-01-14 14:31:11 +0200

    Added last date modified to the footer

--- scripts/date.js
+++ scripts/date.js
@@ -2,3 +2,13 @@ const currentYear = new Date();
 
 const setDate = document.querySelector('#current-year');
 setDate.textContent = currentYear.getFullYear();
+
+document.addEventListener('DOMContentLoaded', function () {
+  if (setDate) {
+    setDate.textContent = new Date().getFullYear();
+  }
+  const lastModified = document.getElementById('last-modified');
+  if (lastModified) {
+    lastModified.textContent = `Last Modified: ${document.lastModified}`;
+  }
+});
